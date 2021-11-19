f4cbf42374a4c1a5f8bb8a427c827e5d2fbaae8a
Philipp Schmitt <pschmitt@users.noreply.github.com>
2021-11-19 23:10:52 +0100

    Remove non-ascii chars from filenames (#2)

--- z-a-patch-dl.plugin.zsh
+++ z-a-patch-dl.plugin.zsh
@@ -4,18 +4,18 @@
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
-autoload -Uz →za-patch-dl-handler
+autoload -Uz za-patch-dl-handler
 
 # An empty stub to fill the help handler fields
-→za-patch-dl-help-null-handler() { :; }
+za-patch-dl-help-null-handler() { :; }
 
 # Register !atclone hook
 @zinit-register-annex "zinit-annex-patch-dl" hook:\!atclone-20 \
-    →za-patch-dl-handler \
-    →za-patch-dl-help-null-handler \
+    za-patch-dl-handler \
+    za-patch-dl-help-null-handler \
     "dl''|patch''" # register a new ice-mod: test''
 
 # Register !atpull hook
 @zinit-register-annex "zinit-annex-patch-dl" hook:\!atpull-20 \
-    →za-patch-dl-handler \
-    →za-patch-dl-help-null-handler
+    za-patch-dl-handler \
+    za-patch-dl-help-null-handler
