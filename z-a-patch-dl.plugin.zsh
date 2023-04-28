2d142178e907b07fbbf9e8c9ab5a950f626d4b36
Vladislav Doster <mvdoster@gmail.com>
2023-04-28 16:46:15 -0500

    fix: log format and help output
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- z-a-patch-dl.plugin.zsh
+++ z-a-patch-dl.plugin.zsh
@@ -14,15 +14,15 @@ za-patch-dl-help-null-handler() { :; }
 
 # Register !atclone hook
 @zinit-register-annex "zinit-annex-patch-dl" \
-  hook:\!atclone-20 \
-  za-patch-dl-handler \
-  za-patch-dl-help-null-handler \
-  "dl''|patch''"
+    hook:\!atclone-20 \
+    za-patch-dl-handler \
+    za-patch-dl-help-null-handler \
+    "dl''|patch''"
 
 # Register !atpull hook
 @zinit-register-annex "zinit-annex-patch-dl" \
-  hook:atpull-20 \
-  za-patch-dl-handler \
-  za-patch-dl-help-null-handler
+    hook:atpull-20 \
+    za-patch-dl-handler \
+    za-patch-dl-help-null-handler
 
 # vim:ft=zsh:sw=2:sts=2:et
